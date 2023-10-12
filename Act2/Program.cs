bef9d52f7b791ca203b62b25a6f1f906a89dcf3d
savanarsa <savanarsa03@gmail.com>
2023-10-13 00:12:51 +0700

    first commit

---
+++ Act2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Act2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Begin
+
+            // Numeric nilM, nilB, rerata
+            double nilM, nilB, rerata;
+
+            // Character status
+            string status;
+
+
+            // Display 'Masukkan Nilai Matematika ='
+            Console.Write("Masukkan Nilai Matematika= ");
+
+            // Accept nilM
+            nilM = Convert.ToDouble(Console.ReadLine());
+
+            // Display 'Masukkan Nilai Bahasa Inggris ='
+            Console.Write("Masukkan Nilai Bahasa Inggris= ");
+
+            // Accept nilB
+            nilB = Convert.ToDouble(Console.ReadLine());
+
+            // Compute rerata = (nilM + nilB) / 2
+            rerata = (nilM + nilB) / 2;
+
+            // if (rerata >= 60 AND nilM >= 70)
+            if (rerata >= 60 && nilM >= 70)
+                //Begin
+
+                    // status = 'lulus'
+                    status = "lulus";
+                //End
+
+            // Else
+            else
+                //Begin
+
+                    // status = 'gagal'
+                    status = "gagal";
+;;            //End
+            // Display 'Status peserta adalah' + status
+            Console.WriteLine("Status peserta adalah {0}", status);
+            Console.ReadKey();
+        }//End
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Class
+{
+    class Inputdata
+    {   //Deklarasi Variabel
+        private double nilM, nilB;
+
+        //method yang berfungsi untuk memasukkan data
+        public void 
+
+            //Display 'Masukkan Nilai Matematika:'
+            InputData 'Nilai Matematika';
+
+            //Accept Nilai Matematika
+            nilai Matematika = Convert.ToDouble (value: Console);
+
+            //Display "Masukkan Nilai Bahasa Inggris: "
+            Inputdata 'Nilai Bahasa Inggris';
+
+            //Accept Nilai Bahasa Inggris
+            nilB Convert.ToDouble(Console.ReadLine);
+    }
+    class prosesdata
+    {//method yang berfungsi memproses data
+        //method yang melewatkan nilai
+        public void
+
+                // Compute rerata = (nilM + nilB) / 2
+                rerata = ( nilM + nilB) / 2;
+
+        //method yang mengembalikan nilai
+        public double
+
+            rerata = (nilM + nilB) / 2
+            return 
+
+        //method yang mengembalikan nilai yang memiliki parameter
+        public double 
+    }
+    class outputdata
+    {//method yang berfungsi untuk menampilkan output data
+        public void
+
+            //Display rerata
+            void.outputdata rerata;
+
+        // method menggunakan parameter
+        public void
+
+            //Display rerata
+            void.outputdata
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //panggil method untuk input data
+            //panggil method untuk proses data
+            //panggil method untuk output data
+        }
+    }
+}
